1490d026d579a5b840e44ada7d0caa7b0660ae05
Mykola Ignatiev <Mykola_Ignatiev@epam.com>
2018-05-30 21:43:10 +0300

    Routing. Task 03. Routes Config

---
+++ routing-demo/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent, AboutComponent, PathNotFoundComponent } from './core';
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: PathNotFoundComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- routing-demo/src/app/core/components/path-not-found/path-not-found.component.ts
+++ routing-demo/src/app/core/components/path-not-found/path-not-found.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-path-not-found',
   templateUrl: './path-not-found.component.html',
   styleUrls: ['./path-not-found.component.css']
 })
