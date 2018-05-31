a51831acd21b7039c629077c14e4a89ca7ba887a
Mykola Ignatiev <Mykola_Ignatiev@epam.com>
2018-05-31 20:20:58 +0300

    Routing. Task 04. Import Routes

--- routing-demo/src/app/app.module.ts
+++ routing-demo/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,12 +14,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
 }
