43bdb35231a97921cd210fc84ce83d724f31c276
Faresis <nikignatiev@gmail.com>
2019-10-14 17:42:38 -0700

    Http demo initial commit

---
+++ http-demo/src/app/users/users.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersServicesModule } from './users-services.module';
+
+import { UserComponent } from './components';
+
+@NgModule({
+  imports: [CommonModule, FormsModule, UsersRoutingModule, UsersServicesModule],
+  declarations: [UsersRoutingModule.components, UserComponent]
+})
+export class UsersModule {}
