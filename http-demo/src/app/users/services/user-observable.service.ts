45ba582e1829eda66addefdf2aac8c42994ce639
Faresis <nikignatiev@gmail.com>
2019-10-15 18:10:39 -0700

    Http. Task 8. User Observable Service

---
+++ http-demo/src/app/users/services/user-observable.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { UserModel } from './../models/user.model';
+import { UsersAPI } from './../users.config';
+import { UsersServicesModule } from '../users-services.module';
+
+@Injectable({
+  providedIn: UsersServicesModule
+})
+export class UserObservableService {
+  constructor(
+    private http: HttpClient,
+    @Inject(UsersAPI) private usersUrl: string
+  ) {}
+
+  getUsers(): Observable<UserModel[]> {
+    return this.http.get<UserModel[]>(this.usersUrl).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+
+  getUser(id: number) {}
+
+  updateUser(user: UserModel) {}
+
+  createUser(user: UserModel) {}
+
+  deleteUser(user: UserModel) {}
+
+  private handleError(err: HttpErrorResponse) {
+    if (err.error instanceof Error) {
+      console.log('An error occured: ', err.error.message);
+    } else {
+      console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+}
+
