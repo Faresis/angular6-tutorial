f87ade7632a850b80538bc38b3fc770af259be4e
Faresis <nikignatiev@gmail.com>
2019-10-24 19:21:43 -0700

    NgRx. Task 08. Add Redux DevTools Extension

--- ngrx-demo/src/app/core/+store/core-store.module.ts
+++ ngrx-demo/src/app/core/+store/core-store.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from './../../../environments/environment';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   declarations: []
 })
