280d89dbe3114db68b4446d431cc6cdd7f2b64b4
Faresis <nikignatiev@gmail.com>
2019-11-01 18:25:09 -0700

    NgRx. Task 19. Router State

--- ngrx-demo/src/app/core/+store/core-store.module.ts
+++ ngrx-demo/src/app/core/+store/core-store.module.ts
@@ -4,13 +4,19 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from './../../../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { RouterStateSerializerProvider, routerReducers } from './router';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(routerReducers),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
+  providers: [
+    RouterStateSerializerProvider,
   ],
   declarations: []
 })
