6127c0fafc501f01d9a5e233960b706fabf71c60
Faresis <nikignatiev@gmail.com>
2019-11-15 15:54:30 -0800

    Forms. Task 02. ReactiveComponent Class

--- forms-demo/src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
+++ forms-demo/src/app/reactive-forms/signup-reactive-form/signup-reactive-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { UserModel } from './../../models/user.model';
 
 @Component({
   selector: 'app-signup-reactive-form',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-reactive-form.component.css']
 })
 export class SignupReactiveFormComponent implements OnInit {
+  countries: Array<string> = ['Ukraine', 'Armenia', 'Belarus', 'Hungary', 'Kazakhstan', 'Poland', 'Russia'];
+  user: UserModel = new UserModel();
+  userForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  onSave() {
+    console.log(this.userForm);
+    console.log(`Saved: ${JSON.stringify(this.userForm.value)}`);
+    console.log(`Saved: ${JSON.stringify(this.userForm.getRawValue())}`);
+  }
+
+  private createForm() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendProducts: new FormControl(true)
+    });
+  }
 }
+
